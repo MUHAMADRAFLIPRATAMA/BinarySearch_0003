bce05be4fdb644d205842906abda1561424b6b57
MUHAMADRAFLIPRATAMA <kipxrap@gmail.com>
2025-05-07 19:42:01 +0700

    input elemen array dari pengguna

--- BinarySeach.cpp
+++ BinarySeach.cpp
@@ -21,3 +21,15 @@ void input()
             cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+
+    cout << "==============================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==============================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "= ";
+        cin >> element[i];
+    }
+}
+
